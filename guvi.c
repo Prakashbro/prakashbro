f2925c1e0e257924f9c6ab861322643622086822
Prakashbro <30368700+Prakashbro@users.noreply.github.com>
2017-08-22 16:15:07 +0530

    Create guvi.c

---
+++ guvi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int num;
+/*number from user*/
+print f(Enter the Number);
+("%d",&num);
+if (num > 0)
+{
+printf (number is positive);
+}
+else if(num < 0)
+{
+printf (Number Is Negative);
+}
+else
+{
+print f(number is zero);
+}
+return 0;
+}
